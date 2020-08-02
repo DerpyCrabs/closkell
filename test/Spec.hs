8c3fadd0714b74333fcb81b1f125a113f29189a5
DerpyCrabs <derpycrabs@gmail.com>
2020-08-02 21:43:44 +0700

    Start to implement test suite for parsing and evaluation

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,47 @@
+import Control.Monad.Except
+import Lib
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Parsing" parsingTests
+  describe "Evaluation" evaluationTests
+
+parsingTests =
+  let test = testTable runParse
+   in do
+        it "parses empty list" $
+          test
+            [ ("()", List Nothing [Atom Nothing "quote", List Nothing []]),
+              ("(   )", List Nothing [Atom Nothing "quote", List Nothing []]),
+              (" () ", List Nothing [Atom Nothing "quote", List Nothing []])
+            ]
+        it "parses integers" $
+          test
+            [ ("20", Integer 20),
+              ("  0x3F  ", Integer 63),
+              ("  0x3f", Integer 63),
+              ("0b101  ", Integer 5),
+              ("0o77", Integer 63)
+            ]
+
+evaluationTests =
+  let test = testTable runEval
+   in do
+        it "evaluates primitive types" $ test [(Integer 1, Right $ Integer 1), (String "test", Right $ String "test")]
+        it "evaluates primitive functions" $ test [(List Nothing [Atom Nothing "+", Integer 1, Integer 2], Right $ Integer 3)]
+
+runEval :: LispVal -> IO (Either LispError LispVal)
+runEval val = runExceptT $ do
+  env <- liftIO primitiveBindings
+  state <- liftIO nullState
+  eval state env val
+
+runParse :: String -> IO LispVal
+runParse = return . extractValue . readExpr ""
+
+testTable :: (Show b, Eq b) => (a -> IO b) -> [(a, b)] -> IO ()
+testTable runTest [] = return ()
+testTable runTest ((input, expected) : tests) = do
+  res <- runTest input
+  (res `shouldBe` expected) >> testTable runTest tests
