917c105c311f8a5d03a70d89729e9f61c4c1992e
DerpyCrabs <derpycrabs@gmail.com>
2020-08-02 21:42:23 +0700

    Implement Eq for LispError and LispVal

--- src/Types.hs
+++ src/Types.hs
@@ -40,6 +40,7 @@ data LispError
   | UnboundVar String String
   | FromCode LispVal
   | Default String
+  deriving (Eq)
 
 data LispVal
   = Atom (Maybe SourcePos) String
@@ -54,3 +55,15 @@ data LispVal
   | IOFunc ([LispVal] -> IOThrowsError LispVal)
   | Port Handle
   | Func {params :: [String], vararg :: Maybe String, body :: [LispVal], closure :: EnvRef}
+
+instance Eq LispVal where
+  (Atom _ s1) == (Atom _ s2) = s1 == s2
+  (List _ l1) == (List _ l2) = l1 == l2
+  (DottedList _ l1 e1) == (DottedList _ l2 e2) = l1 == l2 && e1 == e2
+  (Integer i1) == (Integer i2) = i1 == i2
+  (String s1) == (String s2) = s1 == s2
+  (Float i1) == (Float i2) = i1 == i2
+  (Bool i1) == (Bool i2) = i1 == i2
+  (Port h1) == (Port h2) = h1 == h2
+  (Character c1) == (Character c2) = c1 == c2
+  _ == _ = False
