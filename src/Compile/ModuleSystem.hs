1b8ea894c98de05dd70d896a8983c0ee4f564deb
DerpyCrabs <derpycrabs@gmail.com>
2020-08-11 19:35:32 +0700

    ModuleSystem: don't wrap single expression with do

--- src/Compile/ModuleSystem.hs
+++ src/Compile/ModuleSystem.hs
@@ -5,6 +5,7 @@ import Types
 import Data.Value
 import Parse
 
+moduleSystem (List _ (Atom _ "executable":loadExprs):[expr])  = (:[]) <$> handleLoads loadExprs expr
 moduleSystem (List _ (Atom _ "executable":loadExprs):exprs)  = (:[]) <$> handleLoads loadExprs (func "do" exprs)
 moduleSystem exprs  = moduleSystem $ func "executable" []:exprs
 
