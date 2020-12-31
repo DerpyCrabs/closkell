2faa26694ad7de4c37dab794feefe088cd548b16
DerpyCrabs <derpycrabs@gmail.com>
2020-12-31 13:56:22 +0700

    [EmitJS] Move JS primitives to the Primitive.js file

--- src/Compile/ClosureCompilerPass.hs
+++ src/Compile/ClosureCompilerPass.hs
@@ -2,9 +2,16 @@ module Compile.ClosureCompilerPass where
 
 import System.Command
 
-compilerConfig = ["--language_out", "ECMASCRIPT_2020", "--compilation_level", "ADVANCED"]
+compilerConfig =
+  [ "--language_out",
+    "ECMASCRIPT_2020",
+    "--compilation_level",
+    "ADVANCED",
+    "--warning_level",
+    "QUIET"
+  ]
 
 closureCompilerPass :: String -> IO String
 closureCompilerPass source = do
-  Stdout out <- command [Stdin source] "google-closure-compiler" compilerConfig
+  Stdout out <- command [Stdin source, EchoStderr False] "google-closure-compiler" compilerConfig
   return out
